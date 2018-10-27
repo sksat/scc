7cda58e7d0bf0a0454f71e38b2eb269a30f9534d
sksat <sksat.tec@gmail.com>
2018-10-27 21:01:23 +0900

    add operator "+","-"

--- main.c
+++ main.c
@@ -3,14 +3,38 @@
 
 int main(int argc, char **argv){
 
+	// "1+2"のような簡単な式を処理する
+
 	if(argc != 2){
-		fprintf(stderr, "usage> scc <return value>\n");
+		fprintf(stderr, "usage> scc <expr>\n");
+		return 1;
 	}
 
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("\tmov eax, %d\n", atoi(argv[1]));
+
+	char *p;
+	// 最初の数字
+	printf("\tmov eax, %ld\n", strtol(argv[1], &p, 10));
+
+	while(*p != '\0'){
+		if(*p == '+'){
+			p++; // 読み飛ばす
+			printf("\tadd eax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+		if(*p == '-'){
+			p++;
+			printf("\tsub eax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		// よくわからない文字
+		fprintf(stderr, "unexpected char: %c", *p);
+		return 1;
+	}
+
 	printf("\tret");
 
 	return 0;
--- test.sh
+++ test.sh
@@ -11,10 +11,13 @@ try(){
 		echo "expected $expected, but got $ret"
 		exit -1
 	fi
-	echo "ok[$ret]"
+	echo "ok[$ret = $input]"
 	rm tmp.s tmp.bin
 }
 
 try 0 0
 try 1 1
 try 2 2
+try 3 1+2
+try 6 1+2+3
+try 2 1+2+3-4
