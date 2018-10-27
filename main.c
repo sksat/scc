0f8fdaf034ac1239d678eded55d20618620687d6
sksat <sksat.tec@gmail.com>
2018-10-27 20:09:01 +0900

    return given number

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv){
+
+	if(argc != 2){
+		fprintf(stderr, "usage> scc <return value>\n");
+	}
 
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("\tmov eax, 0\n");
+	printf("\tmov eax, %d\n", atoi(argv[1]));
 	printf("\tret");
 
 	return 0;
